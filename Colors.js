f81f6b35e9602ea53df4d78319eb7766556b710d
Oleksa Sarnatskyi <OlSa@MacBook-Pro-username.local>
2021-04-22 22:55:05 +0300

    Setings up and displaying list (without db)

---
+++ Colors.js
@@ -0,0 +1,7 @@
+export default colors = {
+    black: '#002333',
+    blue: '#038AC6',
+    lightblue: '#75C2E5',
+    white: '#F5F3D3',
+    broun: '#8A4C01'
+}
---
+++ tempData.js
@@ -0,0 +1,70 @@
+// temporary data until connect to db
+
+export default tempData = [
+    {
+        name: 'Plan a trip',
+        color: "#24A6D9",
+        todos: [
+            {
+                title: 'Book flight',
+                completed: true
+            },
+            {
+                title: 'Passport chek',
+                completed: false
+            },
+            {
+                title: 'Push all in github',
+                completed: true
+            },
+            {
+                title: 'Make a video',
+                completed: false
+            }
+        ]
+    },
+    {
+        name: 'BD party',
+        color: "#24A6D9",
+        todos: [
+            {
+                title: 'Bay cake',
+                completed: true
+            },
+            {
+                title: 'Take presents',
+                completed: true
+            },
+            {
+                title: 'Drunk',
+                completed: true
+            },
+            {
+                title: 'Fuck girl',
+                completed: true
+            }
+        ]
+    },
+    {
+        name: 'Nastia',
+        color: "#F18E16",
+        todos: [
+            {
+                title: 'Tak chy nie',
+                completed: true
+            },
+            {
+                title: 'Doslidnytsia',
+                completed: false
+            },
+            {
+                title: 'Drunk',
+                completed: true
+            },
+            {
+                title: 'Scotish bean',
+                completed: false
+            }
+        ]
+    }
+];
